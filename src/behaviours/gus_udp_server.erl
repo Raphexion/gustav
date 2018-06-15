3a1032b06050ca8c2880fed7d90b8bdb0a83b058
Niklas Johansson <raphexion@gmail.com>
2018-06-15 08:03:23 +0200

    gus_udp_server passes local state

--- src/behaviours/gus_udp_server.erl
+++ src/behaviours/gus_udp_server.erl
@@ -1,7 +1,7 @@
 -module(gus_udp_server).
 -behaviour(gen_server).
 
--export([start_link/2]).
+-export([start_link/3]).
 -export([init/1,
 	 handle_call/3,
 	 handle_cast/2,
@@ -10,16 +10,19 @@
 	 code_change/3]).
 
 -callback start_link(any()) -> {ok, pid()}.
--callback decode(any(), any(), any()) -> any().
+-callback decode(any(), any(), any(), any()) -> any().
 
--record(state, {sock, mod}).
+-record(state, {sock, mod, localstate}).
 
-start_link(Mod, Port) ->
-    gen_server:start_link(?MODULE, [Mod, Port], []).
+start_link(Mod, Port, LocalState) ->
+    gen_server:start_link(?MODULE, [Mod, Port, LocalState], []).
 
-init([Mod, Port]) ->
+init([Mod, Port, LocalState]) ->
     {ok, Sock} = gen_udp:open(Port, [binary, {active, true}]),
-    {ok, #state{sock=Sock, mod=Mod}}.
+    {ok, #state{sock=Sock, mod=Mod, localstate=LocalState}}.
+
+handle_call(localstate, _From, State=#state{localstate=LocalState}) ->
+    {reply, {ok, LocalState}, State};
 
 handle_call(What, _From, State) ->
     {reply, {ok, What}, State}.
@@ -27,18 +30,27 @@ handle_call(What, _From, State) ->
 handle_cast(_What, State) ->
     {noreply, State}.
 
-handle_info({udp, _Client, Ip, Port, Data}, State=#state{sock=Sock, mod=Mod}) ->
-    case Mod:decode(Ip, Port, Data) of
-	{reply, Payload} ->
-	    gen_udp:send(Sock, Ip, Port, Payload);
-	{reply, Payload, DPort} ->
-	    gen_udp:send(Sock, Ip, DPort, Payload);
-	{reply, Payload, DIp, DPort} ->
-	    gen_udp:send(Sock, DIp, DPort, Payload);
-	_ ->
-	    ok
-    end,
-    {noreply, State}.
+handle_info({udp, _Client, Ip, Port, Data}, State=#state{sock=Sock, mod=Mod, localstate=LocalState0}) ->
+    LocalState = case Mod:decode(Ip, Port, Data, LocalState0) of
+		     {reply, LocalState1, Payload} ->
+			 gen_udp:send(Sock, Ip, Port, Payload),
+			 LocalState1;
+
+		     {reply, LocalState1, Payload, DPort} ->
+			 gen_udp:send(Sock, Ip, DPort, Payload),
+			 LocalState1;
+
+		     {reply, LocalState1, Payload, DIp, DPort} ->
+			 gen_udp:send(Sock, DIp, DPort, Payload),
+			 LocalState1;
+
+		     {noreply, LocalState1} ->
+			 LocalState1;
+
+		     _ ->
+			 LocalState0
+		 end,
+    {noreply, State#state{localstate=LocalState}}.
 
 terminate(_Reason, #state{sock=Sock}) ->
     gen_udp:close(Sock),
--- src/mock/mock_udp_server.erl
+++ src/mock/mock_udp_server.erl
@@ -2,10 +2,11 @@
 -behaviour(gus_udp_server).
 
 -export([start_link/1]).
--export([decode/3]).
+-export([decode/4]).
 
 start_link(Port) ->
-    gus_udp_server:start_link(?MODULE, Port).
+    gus_udp_server:start_link(?MODULE, Port, #{cnt => 0}).
 
-decode(_Ip, _Port, Data) ->
-    {reply, Data}.
+decode(_Ip, _Port, Data, LocalState) ->
+    {ok, Cnt} = maps:find(cnt, LocalState),
+    {reply, LocalState#{cnt => Cnt + 1}, Data}.
