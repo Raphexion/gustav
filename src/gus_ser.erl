2809c641d8d3241cd4dc42f79112768e93ac92ff
Niklas Johansson <raphexion@gmail.com>
2018-06-25 14:52:01 +0200

    test: handle missing data while unpacking

--- src/gus_ser.erl
+++ src/gus_ser.erl
@@ -23,7 +23,7 @@ packer(Dictionary, BinaryData) ->
 		    case maps:find(Key, Dictionary) of
 			{ok, Value} ->
 			    Next = <<Value:Size>>,
-			    Blob = <<BinaryData/binary, Next/binary>>,
+			    Blob = <<BinaryData/bitstring, Next/bitstring>>,
 			    (packer(Dictionary, Blob))(Tail);
 
 			error ->
@@ -33,18 +33,28 @@ packer(Dictionary, BinaryData) ->
     end.
 
 
+unpacker(Dictionary, <<>>) ->
+    fun(X) ->
+	    case X of
+		[] ->
+		    Dictionary;
+		[{Key, Size} | _Tail] ->
+		    {error, {not_enough_data, {Key, Size}}}
+	    end
+    end;
+
 unpacker(Dictionary, BinaryData) ->
     fun(X) ->
 	    case X of
 		[] ->
 		    Dictionary;
 		[{Key, Size} | Tail] ->
-		    <<Value:Size,
-		      Rest/binary>> = BinaryData,
+		    <<Value:Size, Rest/binary>> = BinaryData,
 		    D = Dictionary#{ Key => Value },
 		    (unpacker(D, Rest))(Tail)
 	    end
     end.
+
 
 %%====================================================================
 %% Internal functions
@@ -59,6 +69,11 @@ packer_minimal_test() ->
     F = packer(#{a => 1, b => 2, c => 3, d => 4}),
     B = F([{a, 8}, {b, 16}, {c, 32}, {d, 8}]),
     ?assert(B =:= <<1:8, 2:16, 3:32, 4:8>>).
+
+packer_minimal_bitstring_test() ->
+    F = packer(#{a => 1, b => 2, c => 3, d => 4}),
+    B = F([{a, 7}, {b, 9}]),
+    ?assert(B =:= <<1:7, 2:9>>).
 
 packer_missing_key_test() ->
     F = packer(#{a => 1}),
@@ -75,6 +90,11 @@ unpacker_minimal_test() ->
     ?assert(maps:find(c, D) =:= {ok, 3}),
     ?assert(maps:find(d, D) =:= {ok, 4}),
     ?assert(maps:find(e, D) =:= error).
+
+unpacker_missing_data_test() ->
+    F = unpacker(#{}, <<1:8>>),
+    D = F([{a, 8}, {b, 16}]),
+    ?assert(D =:= {error, {not_enough_data, {b, 16}}}).
 
 unpacker_remain_prev_test() ->
     F = unpacker(#{e => 5}, <<1:8, 2:16, 3:32, 4:8>>),
